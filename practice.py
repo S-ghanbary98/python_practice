ca63c62fdd277e9cec0f4076412f765fe8a25007
S_ghanbary98 <67980207+Sherv1360@users.noreply.github.com>
2021-07-01 10:35:41 +0100

    more questions

--- practice.py
+++ practice.py
@@ -1,11 +1,22 @@
-# Declare function that takes in a username argument as as string. then returns the name.
+# 1. Declare function that takes in a username argument as as string. then returns the name.
 
 #def username(name):
 #   return name
 
-# Declare a list with number 1 - 5. Iterate through the list and display the list
+# 2. Declare a list with numbers 1 - 5. Iterate through the list and display the list
 
-ls = [1,2, 3,4,5]
+#ls = [1,2, 3,4,5]
 
-for i in ls:
-    print(i)
+#for i in ls:
+#    print(i)
+
+# 3. AND - && & == Which one will return a boolean value.
+#name = "Shervin"
+#if name == "Shervin":
+
+
+# 4. What is the difference between a list and a tuple?
+# Tuples are not mutable ()
+
+# 5. Can we add an element to a list? Can we add an element to a tuple?, Can the element of tuples be of different types?
+# Yes, No and Yes
