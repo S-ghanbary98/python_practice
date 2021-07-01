001d7319024eb63c7f22d65ec823257f910938c0
S_ghanbary98 <67980207+Sherv1360@users.noreply.github.com>
2021-07-01 10:25:21 +0100

    List Iteration

--- practice.py
+++ practice.py
@@ -1,4 +1,11 @@
 # Declare function that takes in a username argument as as string. then returns the name.
 
-def username(name):
-    return name
+#def username(name):
+#   return name
+
+# Declare a list with number 1 - 5. Iterate through the list and display the list
+
+ls = [1,2, 3,4,5]
+
+for i in ls:
+    print(i)
